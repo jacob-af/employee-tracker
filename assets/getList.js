d384325c845a2fc021e29a7a299752d8f2b2a291
jacob-af <jfeitler@gmail.com>
2021-03-02 23:27:36 -0500

    employees functions now separated, getList as well

---
+++ assets/getList.js
@@ -0,0 +1,9 @@
+const getList = async (connection, field, table) => {
+    let list = await connection.awaitQuery(`Select id, ${field} from ${table}`)
+    list = list.map(row => {
+        return { name: row[`${field}`], value: row.id }
+    })
+    return list
+}
+
+module.exports = getList
